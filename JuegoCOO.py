9cfa98b9930cee847eafef6624b2552f54440fc6
Nicolas77gh <ngh25@alu.ua.es>
2026-03-18 19:43:55 +0100

    Implementacion de la clase JuegoCooperativo, juegos por equipos

--- JuegoCOO.py
+++ JuegoCOO.py
@@ -1,9 +1,19 @@
-"""""
 from Juego import Juego
-class JuegoCooperativo(Juego):
-    def __init__(self, nombre, reglas, puntos,personas): # personas es un diccionario con los jugadores y los equipos a los que corresponden
-        super().__init__(nombre, reglas,puntos)
-        self.personas = personas
 
-"""
+class JuegoCooperativo(Juego):
+    def __init__(self, nombre, reglas, puntos, equipos: dict):
+        # equipos: un diccionario donde la clave es el nombre del equipo
+        # y el valor es una lista de objetos Jugador
+        super().__init__(nombre, reglas, puntos)
+        self.__equipos = equipos  # Encapsulamiento (atributo privado)
+
+    def mostrar_info(self):
+        super().mostrar_info()
+        print(f"Modo: Cooperativo por equipos ({len(self.__equipos)} equipos compitiendo)")
+
+    def mostrar_integrantes_equipo(self, nombre_equipo):
+        if nombre_equipo in self.__equipos:
+            integrantes = ", ".join([j.nombre for j in self.__equipos[nombre_equipo]])
+            print(f"Equipo {nombre_equipo}: {integrantes}")
+
 
