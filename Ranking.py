1b28a1386d9d027472a3d5e0ea4f679abfcb09eb
Nicolas77gh <ngh25@alu.ua.es>
2026-03-20 19:46:26 +0100

    Implementacion de la clase JuegoCooperativo, juegos por equipos. Implementacion de la clase JuegoOnline. Implemenatcion de la clase TorneoOnline, herencia multiple de Juego Online y Juego Cooperativo. Pequeñas modificaciones en las clases Coach y Ranking He añadidio metodos especiales para sobrecarga de operadores matematicos y sobrecarga de metodos especiales

--- Ranking.py
+++ Ranking.py
@@ -1,9 +1,7 @@
-# --- archivo: Ranking.py ---
 class Ranking:
     def __init__(self, persona, puesto):
-        self.persona = persona  # Aquí guardamos al objeto Jugador/Coach
+        self.persona = persona
         self.puesto = puesto
 
     def mostrar_posicion(self):
-        # Accedemos a los datos de la persona a través del objeto guardado
         print(f"El jugador {self.persona.nombre} está en el puesto {self.puesto}")
